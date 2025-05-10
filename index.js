938dc751a008368f225264dd0c9e2d735decb1bf
Dhananjayarajan <162829820+Dhananjayarajan@users.noreply.github.com>
2025-05-11 02:51:51 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send({Hi : "Hi there"})
+})
+
+const PORT = process.env.PORT || 5005;
+
+app.listen(PORT);
